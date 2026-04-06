614b472d150c0ae7ed951708935330156cc626a7
Roshan Singh <alcanivorax@users.noreply.github.com>
2026-04-06 19:21:49 +0530

    docs(api): handle api

--- src/app/api/dashboard/trends/route.ts
+++ src/app/api/dashboard/trends/route.ts
@@ -3,7 +3,13 @@ import prisma from "@/src/lib/prisma";
 
 export async function GET(req: NextRequest) {
   try {
-    const records = await prisma.record.findMany();
+    const records = await prisma.record.findMany({
+      select: {
+        amount: true,
+        type: true,
+        date: true,
+      },
+    });
     if (records.length === 0) {
       return NextResponse.json(
         { success: true, message: "No records found" },
@@ -11,7 +17,30 @@ export async function GET(req: NextRequest) {
       );
     }
 
-    return NextResponse.json({ success: true, records }, { status: 200 });
+    const trendsMap: Record<string, { income: number; expense: number }> = {};
+
+    records.forEach((record) => {
+      const month = new Date(record.date).toLocaleString("default", {
+        month: "short",
+      });
+
+      if (!trendsMap[month]) {
+        trendsMap[month] = { income: 0, expense: 0 };
+      }
+
+      if (record.type === "INCOME") {
+        trendsMap[month].income += record.amount;
+      } else {
+        trendsMap[month].expense += record.amount;
+      }
+    });
+
+    const trends = Object.entries(trendsMap).map(([month, data]) => ({
+      month,
+      ...data,
+    }));
+
+    return NextResponse.json({ success: true, data: trends }, { status: 200 });
   } catch (error) {
     return NextResponse.json(
       { success: false, error: "Internal Server Error" },
